4895878d276e2c420553fd66c14a851d40292aa7
snreis <sandranogueirareis@gmail.com>
2019-05-02 11:20:48 +0100

    Update create_artificial_galaxies.py

--- create_artificial_galaxies.py
+++ create_artificial_galaxies.py
@@ -1,116 +1,194 @@
 """
-Program to create artificial galaxies in random fields and positions
+Program to create artificial galaxies in random fields and parameters
 
-By Sandra Nogueira dos Reis, June 2017 @ IA - Lisbon
+Input:
+- Science image
+- Weight rms image
+- Segmentation image from SExtractor
+- PSF image
+Output:
+- The galaxy stamp and its correspondent rms image
+- A catalog containing all the input values for creating the mock images
+
+By Sandra Nogueira dos Reis, July 2017 @ IA - Lisbon
+Updated: March 2019 @ IA - Lisbon
 """
 
 from astropy.io import fits
+from astropy.table import Table
+import montage_wrapper as montage
 import numpy as np
 import os
+import pdb #for debugging purposes only
 
-#--------------------------------paths-----------------------------------------
-images_path    = "/home/sreis/candels_images/"
+# ------------------------------ paths -----------------------------------------
+path_img = "../../../images_candels/"
+path_rms = "../../../images_candels/"
+path_seg = "../../../images_candels/fields_segmentation/"
+path_psf = "../psfs/"
 
-#---------------------------------constants------------------------------------
-zp_imaging = 25.95
-pix_scale  = 0.06 #[arcsec pix^-1] for H-band; for I-band is 0.03
-size_of_fit = 200 #[pix]: for H-band is 200x200 and for I-band is 400x400
-mag_min = 15 #[mag]
-mag_max = 19 #[mag]
-mag_step = 5
-re_min = 0.3/pix_scale #[arcsec]/[arcsec pix^-1] = [pix] => must be in [pix] for GALFIT
-re_max = 3.3/pix_scale #[pix]
-re_step = 10
-nn_min = 2.3
-nn_max = 7.3
-nn_step = 6
-ar_min = 0.4
-ar_max = 1.
-ar_step = 7
-fields_names = ['goodsn','cosmos','uds','egs']
-candels_fields = {'goodsn':images_path+"goodsn_all_wfc3_ir_f160w_060mas_v1.0",'cosmos':images_path+"hlsp_candels_hst_wfc3_cos-tot_f160w_v1.0",'uds':images_path+"hlsp_candels_hst_wfc3_uds-tot_f160w_v1.0","egs":images_path+"egs_all_wfc3_ir_f160w_060mas_v1.0"}
-#------------------------------------------------------------------------------
+# ----------------------------- variables --------------------------------------
+name_img = ["hlsp_candels_hst_wfc3_gn-tot-60mas_f160w_v1.0_drz.fits",  # GOODS-N
+            "hlsp_candels_hst_wfc3_cos-tot_f160w_v1.0_drz.fits",       # COSMOS
+            "hlsp_candels_hst_wfc3_uds-tot_f160w_v1.0_drz.fits",       # UDS
+            "hlsp_candels_hst_wfc3_egs-tot-60mas_f160w_v1.0_drz.fits"] # EGS
 
-mag_range = np.linspace(mag_min,mag_max,mag_step) #linspace return evenly spaced numbers over a specified interval
-re_range = np.linspace(re_min,re_max,re_step)
-nn_range = np.linspace(nn_min,nn_max,nn_step)
-ar_range = np.linspace(ar_min,ar_max,ar_step)
+name_rms = ["hlsp_candels_hst_wfc3_gn-tot-60mas_f160w_v1.0_rms.fits",  # GOODS-N
+            "hlsp_candels_hst_wfc3_cos-tot_f160w_v1.0_rms.fits",       # COSMOS
+            "hlsp_candels_hst_wfc3_uds-tot_f160w_v1.0_rms.fits",       # UDS
+            "hlsp_candels_hst_wfc3_egs-tot-60mas_f160w_v1.0_rms.fits"] # EGS
 
-count = 0 #to count the iterations
-for mag_ele in mag_range:
-    for re_ele in re_range:
-        for nn_ele in nn_range:
-            for ar_ele in ar_range:
-                count = count+1
-                field = np.random.choice(fields_names) #choose a random field
-                seg_img = fits.open(field+"_segmentation.fits")
-                seg_data = seg_img[0].data #read the values from segmentation file
-                seg_hdr = seg_img[0].header
-                image_of_field = candels_fields[field]+"_drz.fits"
-                rms_of_field = candels_fields[field]+"_rms.fits"
-                #print count, field, image_of_choosen_field
+name_seg = ["goodsn_segmentation.fits", # GOODS-N
+            "cosmos_segmentation.fits", # COSMOS
+            "uds_segmentation.fits",    # UDS
+            "egs_segmentation.fits"]    # EGS
 
-                #-----------------take values from the header-------------------
-                exptime = seg_hdr['EXPTIME']
-                x_naxis = seg_hdr['NAXIS1']
-                y_naxis = seg_hdr['NAXIS2']
+name_psf = "new_psf_hband.fits"
 
-                #-----------------------------read catalogs---------------------
-                #it is possible to read SExtractor tables with the astropy.io.ascii module:
-                #from astropy.io import ascii
-                #data=ascii.read("/home/sreis/simulations/goodsn.cat")
-                field_cat = field+".cat"
-                number,xx,yy,mag_aper,flux_aper,fluxerr_aper,flux_auto,fluxerr_auto,ra,dec,fwhm,sm_sex,xmin,xmax,ymin,ymax,ellip,theta = np.loadtxt(field_cat, unpack = True)
+exptimes   = {'goodsn': 284527.6, 'cosmos': 130415.1, 'uds': 135114.9, 'aegis': 159257.8}
+x_naxis    = {'goodsn':    20480, 'cosmos':    14000, 'uds':    30720, 'aegis':    40800}
+y_naxis    = {'goodsn':    20480, 'cosmos':    36000, 'uds':    12800, 'aegis':    12600}
+zp_imaging = {'goodsn':  25.9463, 'cosmos':  25.9463, 'uds':    25.96, 'aegis':    25.96} # taken from the official CANDELS readme files
 
-                #---------------parameters to create artificial galaxy----------
-                gal_zp  = zp_imaging - 2.5 * np.log10 (exptime)
-                #---galaxy position---
-                # x_pix = np.random.randint(200,x_naxis-199) #random.randint return random INTEGERS from low (inclusive) to high (exclusive); I use 200 for low value to avoid edges
-                # y_pix = np.random.randint(200,y_naxis-199)
-                x_pix = np.random.uniform(200.,x_naxis-199.)
-                y_pix = np.random.uniform(200.,y_naxis-199.)
-                xmin = x_pix - size_of_fit/2.
-                xmax = x_pix + size_of_fit/2.
-                ymin = y_pix - size_of_fit/2.
-                ymax = y_pix + size_of_fit/2.
-                pa_value = np.random.uniform(-90,91) #any value within the given interval is equally likely to be drawn by uniform
+pix_scale   = 0.06            # [arcsec/pix]
+size_of_fit = 300             # [pix]
+side        = size_of_fit - 1 # [pix]
 
-                #--------------------create galfit script----------------------
-                if seg_data[y_pix,x_pix] == 0:
-                    file = open(str(count)+"_artificial_galaxy.script", "w")
-                    file.write("# IMAGE PARAMETERS\n")
-                    file.write(" A) "+image_of_field+"  # Input Data image (FITS file)\n")
-                    file.write(" B) "+str(count)"_artificial_galaxy.fits  # Name for the output image\n")
-                    file.write(" C) "+rms_of_field+"  # Noise image name (made from data if blank or 'none')\n")
-                    file.write(" D) "+images_path+"gs_deep_f160w_v0.5_psf.fits  # Input PSF image and (optional) diffusion kernel\n")
-                    file.write(" E) 1  # PSF oversampling factor relative to data\n")
-                    file.write(" F) none  # Pixel mask (ASCII file or FITS file with non-0 values)\n")
-                    file.write(" G) none  # Parameter constraint file (ASCII)\n")
-                    file.write(" H) "+str(xmin)+" "+str(xmax)+" "+str(ymin)+" "+str(ymax)+" # Image region to fit (xmin xmax ymin ymax)\n")
-                    file.write(" I) "+str(size_of_fit)+" "+str(size_of_fit)+"  # Size of convolution box (x y)\n")
-                    file.write(" J) "+str(gal_zp)+"  # Magnitude photometric zeropoint\n")
-                    file.write(" K) "+str(pix_scale)+" "+str(pix_scale)+"  # Plate scale (dx dy)\n")
-                    file.write(" O) regular  # Display type (regular, curses, both)\n")
-                    file.write(" P) 1  # Create ouput only? (1=yes; 0=optimize)\n") #item P set to 1: GALFIT will create a model image based on your input parameters and immediately quit
-                    file.write(" S) 0  # Modify/create objects interactively?\n")
-                    file.write("\n")
-                    file.write("\n")
-                    file.write("# Component number: 1\n")
-                    file.write(" 0) sky                    #  Component type\n")
-                    file.write(" 1) 0.00      1       #  Sky background at center of fitting region [ADUs]\n")
-                    file.write(" 2) 0.000     1       #  dsky/dx (sky gradient in x)     [ADUs/pix]\n")
-                    file.write(" 3) 0.000     1       #  dsky/dy (sky gradient in y)     [ADUs/pix]\n")
-                    file.write(" Z) 0                      #  Skip this model in output image?  (yes=1, no=0)\n")
-                    file.write("\n")
-                    #-----singlefit-----
-                    file.write("# Component number: 2 - galaxy\n")
-                    file.write(" 0) sersic                 #  Component type\n")
-                    file.write(" 1) "+str(x_pix)+" "+str(y_pix)+" 1 1  #  Position x, y\n")
-                    file.write(" 3) "+str(mag_ele)+"     1          #  Integrated magnitude\n")
-                    file.write(" 4) "+str(re_ele)+"      1          #  R_e (effective radius)   [pix]\n")
-                    file.write(" 5) "+str(nn_ele)+"           1          #  Sersic index n (de Vaucouleurs n=4)\n")
-                    file.write(" 9) "+str(ar_ele)+"      1          #  Axis ratio (b/a)  \n")
-                    file.write("10) "+str(pa_value)+"    1          #  Position angle (PA) [deg: Up=0, Left=90]\n")
-                    file.write(" Z) 0                      #  Skip this model in output image?  (yes=1, no=0)\n")
-                    file.write("\n")
-                    file.close()
+mag_min = 15.5          # [mag]
+mag_max = 19            # [mag]
+re_min  = 0.3/pix_scale # [arcsec]/[arcsec pix^-1] = [pix] => must be in [pix] for GALFIT
+re_max  = 3./pix_scale  # [pix]
+nn_min  = 1.
+nn_max  = 6.
+ar_min  = 0.3
+ar_max  = 1.
+
+number_of_simulated_gals = 4500                         # because 7*9*10*7 (steps for each parameter) = 4410, so I round it up to 4500
+number_of_gals_per_field = number_of_simulated_gals / 4 # divided by 4 because I have 4 fields
+# ------------------------------------------------------------------------------
+
+
+jj = 0
+gal_id    = []
+xx_center = []
+yy_center = []
+mag_in    = []
+re_in     = []
+nn_in     = []
+ar_in     = []
+pa_in     = []
+# ----- for each mosaic to cut -----
+for ii in range(len(name_img)):
+    # --- detecting the mosaic's patch of the sky ---
+    if name_img[ii].find("gn")    != -1:
+        field = "goodsn"
+    elif name_img[ii].find("cos") != -1:
+        field = "cosmos"
+    elif name_img[ii].find("uds") != -1:
+        field = "uds"
+    elif name_img[ii].find("egs") != -1:
+        field = "aegis"
+
+    # ----- obtaining the galaxy coordinates in pixels -----
+    image_of_field = path_img + name_img[ii]
+    image_of_rms   = path_img + name_rms[ii]
+    field_img      = fits.open (image_of_field)
+    seg_img        = fits.open (path_seg + name_seg[ii])
+    # print "field image ", name_img[ii], ", segmentation image ", name_seg[ii]
+
+    # ----- parameters to create artificial galaxy -----
+    for kk in range(number_of_gals_per_field):
+        gal     = jj + 1                                                  # because I don't want a galaxy with name zero
+        gal_zp  = zp_imaging[field] - 2.5 * np.log10 ( exptimes[field] )
+        mag     = np.random.uniform (mag_min, mag_max)                    # any value within the given interval is equally likely to be drawn by uniform
+        re      = np.random.uniform (re_min,   re_max)
+        nn      = np.random.uniform (nn_min,   nn_max)
+        ar      = np.random.uniform (ar_min,   ar_max)
+        pa      = np.random.uniform (-90,          91)
+
+        # --- galaxy position ---
+        x_pix = np.random.uniform (size_of_fit, x_naxis[field] - size_of_fit); x_pix = int(x_pix)
+        y_pix = np.random.uniform (size_of_fit, y_naxis[field] - size_of_fit); y_pix = int(y_pix)
+
+        # --- galaxy stamp ---
+        xmin = int(x_pix - size_of_fit/2.)
+        xmax = int(x_pix + size_of_fit/2.) - 1
+        ymin = int(y_pix - size_of_fit/2.)
+        ymax = int(y_pix + size_of_fit/2.) - 1
+        stamp = field_img[0].data[ ymin:ymax+1, xmin:xmax+1 ]
+
+        # --- if it is a position of a sextracted object or a zero value pixel, choose another position ---
+        while not stamp.all() and (seg_img[0].data[y_pix, x_pix] != 0 or field_img[0].data[y_pix, x_pix] == 0):
+            x_pix = np.random.uniform (size_of_fit, x_naxis[field] - size_of_fit); x_pix = int(x_pix)
+            y_pix = np.random.uniform (size_of_fit, y_naxis[field] - size_of_fit); y_pix = int(y_pix)
+            xmin  = int(x_pix - size_of_fit/2.)
+            xmax  = int(x_pix + size_of_fit/2.) - 1
+            ymin  = int(y_pix - size_of_fit/2.)
+            ymax  = int(y_pix + size_of_fit/2.) - 1
+            stamp = field_img[0].data[ ymin:ymax+1, xmin:xmax+1 ]
+
+        # ----- create stamp with original image (without model galaxy) -----
+        xx = xmin + 1
+        yy = ymin + 1
+        montage.mSubimage_pix (image_of_field, str(gal)+"_original_stamp.fits", xx, yy, side)
+        montage.mSubimage_pix (image_of_rms, str(gal)+"_rms.fits", xx, yy, side)
+
+
+        # ----- write galfit script to create model galaxy -----
+        file = open(str(gal)+"_artificial_galaxy.script", "w")
+        file.write("# IMAGE PARAMETERS\n")
+        file.write(" A) "+str(gal)+"_original_stamp.fit  # Input Data image (FITS file)\n")
+        file.write(" B) "+str(gal)+"_artificial_galaxy.fits  # Name for the output image\n")
+        file.write(" C) "+str(gal)+"_rms.fits  # Noise image name (made from data if blank or 'none')\n")
+        file.write(" D) "+path_psf+name_psf+" # Input PSF image and (optional) diffusion kernel\n")
+        file.write(" E) 1  # PSF oversampling factor relative to data\n")
+        file.write(" F) none  # Pixel mask (ASCII file or FITS file with non-0 values)\n")
+        file.write(" G) none  # Parameter constraint file (ASCII)\n")
+        file.write(" H) "+str(xmin)+" "+str(xmax)+" "+str(ymin)+" "+str(ymax)+" # Image region to fit (xmin xmax ymin ymax)\n")
+        file.write(" I) "+str(size_of_fit)+" "+str(size_of_fit)+"  # Size of convolution box (x y)\n")
+        file.write(" J) "+str(gal_zp)+"  # Magnitude photometric zeropoint\n")
+        file.write(" K) "+str(pix_scale)+" "+str(pix_scale)+"  # Plate scale (dx dy)\n")
+        file.write(" O) regular  # Display type (regular, curses, both)\n")
+        file.write(" P) 1  # Create ouput only? (1=yes; 0=optimize)\n") # item P set to 1: GALFIT will create a model image based on your input parameters and immediately quit
+        file.write(" S) 0  # Modify/create objects interactively?\n")
+        file.write("\n")
+        file.write("\n")
+
+        # --- singlefit ---
+        file.write("# Component number: 1 - galaxy\n")
+        file.write(" 0) sersic                 #  Component type\n")
+        file.write(" 1) "+str(x_pix)+" "+str(y_pix)+" 1 1  #  Position x, y\n")
+        file.write(" 3) "+str(mag)+"     1          #  Integrated magnitude\n")
+        file.write(" 4) "+str(re)+"      1          #  R_e (effective radius)   [pix]\n")
+        file.write(" 5) "+str(nn)+"           1          #  Sersic index n (de Vaucouleurs n=4)\n")
+        file.write(" 9) "+str(ar)+"      1          #  Axis ratio (b/a)  \n")
+        file.write("10) "+str(pa)+"    1          #  Position angle (PA) [deg: Up=0, Left=90]\n")
+        file.write(" Z) 0                      #  Skip this model in output image?  (yes=1, no=0)\n")
+        file.write("\n")
+        file.close()
+
+
+        # ----- run galfit to create model galaxy -----
+        os.system("galfit "+str(gal)+"_artificial_galaxy.script")
+
+        # ----- adding the model to the real image -----
+        img_model  = fits.open (str(gal)+"_artificial_galaxy.fits") # open model galaxy I just created with GALFIT
+        model_data = img_model[0].data
+
+        # --- add model galaxy to the original image ---
+        stamp_header = fits.getheader (str(gal)+"_original_stamp.fits")
+        final_img    = stamp + model_data
+        fits.writeto (str(gal)+"_mock_galaxy.fits", final_img, header = stamp_header, overwrite = True) # save final fits stamp, with original image from the CANDELS field + model galaxy
+        jj = jj + 1
+
+        # ----- saving everything in the catalog -----
+        gal_id.append(gal)
+        xx_center.append(xx)
+        yy_center.append(yy)
+        mag_in.append(mag)
+        re_in.append(re)
+        nn_in.append(nn)
+        ar_in.append(ar)
+        pa_in.append(pa)
+    new_table = Table([gal_id,xx_center,yy_center,mag_in,re_in,nn_in,ar_in,pa_in],names=['gal','x','y','mag','re','n','ar','pa'])
+new_table.write("catalog_mock_galaxies_hband.cat", formats = {'mag':'%0.4f', 're':'%0.4f', 'n':'%0.4f', 'ar':'%0.4f', 'pa':'%0.4f'}, format = "ascii.commented_header", overwrite = True)
